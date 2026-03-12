27e52fc4c72e009576dc69f6e78d88aa15d68afe
EngrHenry111 <henryengrakpan@gmail.com>
2026-03-12 20:03:30 +0100

    adding sitemap

--- controllers/sitemapController.js
+++ controllers/sitemapController.js
@@ -7,32 +7,50 @@ export const generateSitemap = async (req, res) => {
 
   const tutorials = await Tutorial.find();
 
-  const sitemap = new SitemapStream({
-   hostname: "student-toolsngsch.vercel.app"
+  const smStream = new SitemapStream({
+   hostname: "https://student-toolsngsch.vercel.app"
   });
 
   // Static pages
-  sitemap.write({ url: "/", changefreq: "daily", priority: 1 });
-  sitemap.write({ url: "/tutorials", changefreq: "weekly", priority: 0.8 });
-  sitemap.write({ url: "/about", changefreq: "monthly", priority: 0.5 });
-  sitemap.write({ url: "/contact", changefreq: "monthly", priority: 0.5 });
-  sitemap.write({ url: "/privacy-policy", changefreq: "monthly", priority: 0.3 });
+  const staticPages = [
+   "/",
+   "/tutorials",
+   "/cgpa-calculator",
+   "/waec-calculator",
+   "/jamb-calculator",
+   "/study-planner",
+   "/scholarships",
+   "/about",
+   "/contact",
+   "/privacy-policy",
+   "/terms",
+   "/author"
+  ];
+
+  staticPages.forEach(page => {
+   smStream.write({
+    url: page,
+    changefreq: "weekly",
+    priority: 0.8
+   });
+  });
 
   // Dynamic tutorial pages
-  tutorials.forEach((tutorial) => {
-   sitemap.write({
+  tutorials.forEach(tutorial => {
+   smStream.write({
     url: `/tutorial/${tutorial.slug}`,
     changefreq: "weekly",
     priority: 0.7
    });
   });
 
-  sitemap.end();
+  smStream.end();
 
-  const xml = await streamToPromise(sitemap);
+  const sitemapOutput = await streamToPromise(smStream);
 
   res.header("Content-Type", "application/xml");
-  res.send(xml.toString());
+
+  res.send(sitemapOutput.toString());
 
  } catch (error) {
 
@@ -43,3 +61,51 @@ export const generateSitemap = async (req, res) => {
  }
 
 };
+
+
+
+// import { SitemapStream, streamToPromise } from "sitemap";
+// import Tutorial from "../models/tutorialModel.js";
+
+// export const generateSitemap = async (req, res) => {
+
+//  try {
+
+//   const tutorials = await Tutorial.find();
+
+//   const sitemap = new SitemapStream({
+//    hostname: "student-toolsngsch.vercel.app"
+//   });
+
+//   // Static pages
+//   sitemap.write({ url: "/", changefreq: "daily", priority: 1 });
+//   sitemap.write({ url: "/tutorials", changefreq: "weekly", priority: 0.8 });
+//   sitemap.write({ url: "/about", changefreq: "monthly", priority: 0.5 });
+//   sitemap.write({ url: "/contact", changefreq: "monthly", priority: 0.5 });
+//   sitemap.write({ url: "/privacy-policy", changefreq: "monthly", priority: 0.3 });
+
+//   // Dynamic tutorial pages
+//   tutorials.forEach((tutorial) => {
+//    sitemap.write({
+//     url: `/tutorial/${tutorial.slug}`,
+//     changefreq: "weekly",
+//     priority: 0.7
+//    });
+//   });
+
+//   sitemap.end();
+
+//   const xml = await streamToPromise(sitemap);
+
+//   res.header("Content-Type", "application/xml");
+//   res.send(xml.toString());
+
+//  } catch (error) {
+
+//   res.status(500).json({
+//    message: error.message
+//   });
+
+//  }
+
+// };
--- routes/sitemapRoutes.js
+++ routes/sitemapRoutes.js
@@ -6,3 +6,14 @@ const router = express.Router();
 router.get("/sitemap.xml", generateSitemap);
 
 export default router;
+
+
+
+// import express from "express";
+// import { generateSitemap } from "../controllers/sitemapController.js";
+
+// const router = express.Router();
+
+// router.get("/sitemap.xml", generateSitemap);
+
+// export default router;
