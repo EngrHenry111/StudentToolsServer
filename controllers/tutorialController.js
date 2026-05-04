0c1252d61e49f0c6ea6d32b3b87007ab9dc22732
EngrHenry111 <henryengrakpan@gmail.com>
2026-05-04 12:08:33 +0100

    subtopics

--- controllers/tutorialController.js
+++ controllers/tutorialController.js
@@ -402,24 +402,15 @@ export const getSubtopics = async (req, res) => {
   try {
     const { category, topic } = req.query;
 
-    if (!category || !topic) {
-      return res.status(400).json({ message: "Category and topic required" });
-    }
-
     const tutorials = await Tutorial.find({
       category,
       topic
     }).select("title slug");
 
-    // Extract subtopics (titles)
-    const subtopics = tutorials.map(t => ({
-      title: t.title,
-      slug: t.slug
-    }));
-
-    res.json(subtopics);
+    res.json(tutorials);
 
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: error.message });
   }
 };
