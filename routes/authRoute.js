2acb81d150d847281509610a18131ff132fc1da6
EngrHenry111 <henryengrakpan@gmail.com>
2026-08-11 23:06:53 +0100

    login resolve

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { registerUser, loginUser 
-    ,logoutUser
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  verifyEmail,
+  getMe
 } from "../controllers/authController.js";
+import authUser from "../middleware/authUser.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/refresh", refreshAccessToken);
+router.get("/verify/:token", verifyEmail);
+router.get("/me", authUser, getMe);
 
 export default router;
